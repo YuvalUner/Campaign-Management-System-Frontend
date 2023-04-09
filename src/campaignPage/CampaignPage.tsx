71c4d2f23e64d69307d3dcd278cbb9f7c0a9e33e
Yuval Uner <yuvaluner@gmail.com>
2023-04-09 14:33:09 +0300

    [CMP-279] Started working on a proper campaign page. Added 401 error page.

--- src/campaignPage/CampaignPage.tsx
+++ src/campaignPage/CampaignPage.tsx
@@ -1,19 +1,52 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import ServerRequestMaker from "../utils/server-request-maker";
+import config from "../app-config.json";
+import {HttpStatusCode} from "axios";
+import {Box, CircularProgress} from "@mui/material";
+import NotAuthorizedPage from "../notAuthorizedPage/notAuthorizedPage";
 
 function CampaignPage(): JSX.Element {
 
     const params = useParams();
+    const campaignGuid = params.campaignGuid;
+    const [loading, setLoading] = useState(true);
+    const [enteredCampaign, setEnteredCampaign] = useState(false);
 
     useEffect(() => {
-        const campaignGuid = params.campaignGuid;
-        console.log("Campaign Guid: " + campaignGuid);
-    });
+        ServerRequestMaker.MakePostRequest(
+            config.ControllerUrls.Campaigns.Base + config.ControllerUrls.Campaigns.Enter + campaignGuid,
+            {}
+        ).then((res) => {
+            if (res.status === HttpStatusCode.Ok){
+                setEnteredCampaign(true);
+            }
+        }).finally(() => {
+            setLoading(false);
+        });
+    }, []);
+
+    const renderMainPage = (): JSX.Element => {
+        return (
+            !enteredCampaign ?
+                <NotAuthorizedPage errorMessage={"You are not a member of this campaign or you are not logged in"}/>
+                : <h1>Entered campaign</h1>
+        );
+    };
 
     return (
-        <div>
-            <h1>Campaign Page</h1>
-        </div>
+        <Box sx={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            {loading ?
+                <CircularProgress/>
+                : renderMainPage()
+            }
+        </Box>
     );
 }
 
