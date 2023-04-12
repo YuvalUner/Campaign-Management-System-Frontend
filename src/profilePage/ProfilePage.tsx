2c36f5fb3562cabac51526f7de15cad83583a89c
Maxim <maxim.grinblat@gmail.com>
2023-04-12 14:06:52 +0300

    [CMP-85] Finishing working on profile page

--- src/profilePage/ProfilePage.tsx
+++ src/profilePage/ProfilePage.tsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from "react";
 import UserPrivateInfo from "../models/user-private-info";
-import GenericRequestMaker from "../utils/generic-request-maker";
 import config from "../app-config.json";
-import {HttpStatusCode} from "axios";
+import {AxiosResponse, HttpStatusCode} from "axios";
 import ErrorCodeExtractor from "../utils/error-code-extractor";
-import { Alert, AlertTitle } from "@mui/material"; // For Material-UI
+import {Alert, AlertTitle, FormControl, InputLabel,
+    MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material"; 
+import ServerRequestMaker from "../utils/server-request-maker";
 //import { Alert, AlertTitle } from "react-bootstrap"; // For Bootstrap
 
 function ProfilePage(): JSX.Element {
@@ -23,35 +24,40 @@ function ProfilePage(): JSX.Element {
     //set Alert definition
     const [alertMessage, setAlertMessage] = useState<React.ReactNode>(null);
 
+    const [cities, setCities] = useState([{cityId : 1, cityName : "Jerusalem"}]);
+
     useEffect(() => {
-        GenericRequestMaker.MakeGetRequest(
+        ServerRequestMaker.MakeGetRequest(
             config.ControllerUrls.Users.Base + config.ControllerUrls.Users.GetVerifiedStatus
-        ).then((res) => {
+        ).then((res: AxiosResponse) => {
             setIsVerified(res.data.isVerified === true);
+            // setIsVerified(true);
         });
     }, []);
 
     useEffect(() => {
-        GenericRequestMaker.MakeGetRequest(
+        ServerRequestMaker.MakeGetRequest(
             config.ControllerUrls.Users.Base + config.ControllerUrls.Users.GetProfilePageInfo
-        ).then((res) => {
+        ).then((res: AxiosResponse) => {
             const updatedUserDetails = {
                 firstNameHeb: res.data.firstNameHeb,
                 lastNameHeb: res.data.lastNameHeb,
-                idNumber: -1,
+                idNumber: 0,
                 cityName: res.data.cityName,
             };
-            setUserDetails(updatedUserDetails);
+            if(isVerified) {
+                setUserDetails(updatedUserDetails);
+            }
+        });
+    }, [isVerified]);
+
+    useEffect(() => {
+        ServerRequestMaker.MakeGetRequest(
+            config.ControllerUrls.Cities.Base
+        ).then((res: AxiosResponse) => {
+            setCities(res.data);
         });
     }, []);
-
-    /*useEffect(() => {
-        GenericRequestMaker.MakeGetRequest(
-            config.ServerBaseUrl + config.ControllerUrls.Cities.Base + "GetAllCities"
-        ).then((res) => {
-
-        });
-    }, []);*/
 
     //Define a show alert function
     const showAlert = (message: string, severity: "error" | "warning" | "info" | "success") => {
@@ -70,25 +76,21 @@ function ProfilePage(): JSX.Element {
             showAlert("Please fill in all fields", "error");
             return;
         }
-        if (userDetails.firstNameHeb.match(/^[\u0590-\u05FF ]+$/) === null) {
-            showAlert("First name must be in Hebrew letters only", "error");
-            return;
-        }
-        if (userDetails.lastNameHeb.match(/^[\u0590-\u05FF ]+$/) === null) {
-            showAlert("Last name must be in Hebrew letters only", "error");
-            return;
-        }
-        if (userDetails.cityName.match(/^[a-zA-Z ]+$/) === null) {
-            showAlert("City name must be in English letters only", "error");
-            return;
-        }
+        // if (userDetails.firstNameHeb.match(/^[\u0590-\u05FF ]+$/) === null) {
+        //     showAlert("First name must be in Hebrew letters only", "error");
+        //     return;
+        // }
+        // if (userDetails.lastNameHeb.match(/^[\u0590-\u05FF ]+$/) === null) {
+        //     showAlert("Last name must be in Hebrew letters only", "error");
+        //     return;
+        // }
         if (userDetails.idNumber.toString().length !== 9) {
             showAlert("ID number must be exactly 9 digits", "error");
             return;
         }
-        setSubmitted(true);
-
-        const res = await GenericRequestMaker.MakePutRequest(
+        
+        
+        const res = await ServerRequestMaker.MakePutRequest(
             config.ControllerUrls.Users.Base + config.ControllerUrls.Users.UserPrivateInfo,
             userDetails
         );
@@ -96,6 +98,7 @@ function ProfilePage(): JSX.Element {
         if (res.status === HttpStatusCode.Ok){
             showAlert("User private info updated successfully!", "success");
             setIsVerified(true);
+            setSubmitted(true);
         } else{
             const errNum = ErrorCodeExtractor(res.data);
             if (res.status !== HttpStatusCode.Unauthorized) {
@@ -127,6 +130,16 @@ function ProfilePage(): JSX.Element {
         }));
     };
 
+    const handleCityChange = (event: SelectChangeEvent<number>) => {
+        const { value } = event.target;
+        const cityName = cities.find(city => city.cityId === value)?.cityName || "";
+        console.log(event.target);
+        setUserDetails((prevState) => ({
+            ...prevState,
+            cityName: cityName,
+        }));
+    };
+
     return (
         <div>
             <h1>Profile Page</h1>
@@ -135,7 +148,7 @@ function ProfilePage(): JSX.Element {
                 <div>
                     <p>Name: {userDetails.firstNameHeb}</p>
                     <p>Surname: {userDetails.lastNameHeb}</p>
-                    <p>Address: {userDetails.cityName}</p>
+
                 </div>
             ) : (
                 // If the form hasn't been submitted yet, display the form for the user to enter their personal details
@@ -144,7 +157,7 @@ function ProfilePage(): JSX.Element {
                     <form onSubmit={handleSubmit}>
                         <label>
                         Name:
-                            <input
+                            <TextField
                                 type="text"
                                 name="firstNameHeb"
                                 value={userDetails.firstNameHeb}
@@ -153,7 +166,7 @@ function ProfilePage(): JSX.Element {
                         </label>
                         <label>
                         Last Name:
-                            <input
+                            <TextField
                                 type="text"
                                 name="lastNameHeb"
                                 value={userDetails.lastNameHeb}
@@ -162,7 +175,7 @@ function ProfilePage(): JSX.Element {
                         </label>
                         <label>
                         ID:
-                            <input
+                            <TextField
                                 type="text"
                                 name="idNumber"
                                 value={userDetails.idNumber}
@@ -170,13 +183,24 @@ function ProfilePage(): JSX.Element {
                             />
                         </label>
                         <label>
-                        City:
-                            <input
-                                type="text"
-                                name="cityName"
-                                value={userDetails.cityName}
-                                onChange={handleInputChange}
-                            />
+                            <p><FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">City</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={
+                                        cities.find(city => city.cityName === userDetails.cityName)?.cityId
+                                        || cities[0].cityId
+                                    }
+                                    label="City"
+                                    onChange={handleCityChange}
+                                >
+                                    {cities.map(city => {
+                                        // eslint-disable-next-line max-len
+                                        return <MenuItem key={city.cityId} value={city.cityId}>{city.cityName}</MenuItem>;
+                                    })}
+                                </Select>
+                            </FormControl></p>
                         </label>
                         <button type="submit">Submit</button>
                     </form>
